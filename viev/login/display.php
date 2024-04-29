703a9f5d34772630ac8f2936c05bb5047bf48e43
YAO <yamin13y@gmail.com>
2024-04-29 18:46:24 +0200

    [task] création d'un formulaire en html

---
+++ viev/login/display.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <main>
+        <form action="/ctrl/login/login.php" method="post">
+            <!-- E-mail -->
+            <div>
+                <label for="code">E-mail :</label>
+                <input type="text" name="mail" id="code">
+            </div>
+            <!-- PASSWORD -->
+            <div>
+                <label for="label">Mot de passe :</label>
+                <input type="password" name="password" id="label">
+            </div>
+            <div class="submit">
+                <button type="submit">Valider</button>
+            </div>
+        </form>
+    </main>
+</body>
+</html>
