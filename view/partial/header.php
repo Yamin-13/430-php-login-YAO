d83eb46976e7f470e563fa861e1bfc480d34a956
yamin <yam@live.fr>
2024-05-27 21:54:40 +0200

    [feature] affichage du nom de l'utilisateur à coté de l'image log out

--- view/partial/header.php
+++ view/partial/header.php
@@ -1,28 +1,26 @@
 <?php
-
-
 // Vérifie si l'utilisateur est authentifié
-$isLoggedIn = isset($_SESSION['user']);
-?>
-
+$isLoggedIn = isset($_SESSION['user']); ?>
 
 <header>
+    <h1>Fleur de Dahlia</h1>
     <nav>
-        <h1>Fleur de Dahlia</h1>
         <ul>
             <li><a href="#">Accueil</a></li>
             <li><a href="/ctrl/login/welcome.php">Welcome</a></li>
             <li><a href="#">Nos Produits</a></li>
-            <li><a href="/ctrl/login/secret.php">Nos Secrets</a></li>
+            <?php if ($isLoggedIn && $_SESSION['user']['idRole'] == '10') : ?> <!-- cache le lien Nos secret si l'utilisateur n'a pas le role admin -->
+                <li><a href="/ctrl/login/secret.php">Nos Secrets</a></li>
+            <?php endif; ?>
         </ul>
- 
-        <?php if ($isLoggedIn): ?>
-            <a href="/ctrl/login/logout.php" id="profileIconLogout" class="profile-icon"><img src="/asset/img/flowerYelow-removebg-preview.png" alt=""></a>
-            <?php else: ?>
-                <a href="" id="profileIcon" class="profile-icon"><img src="/asset/img/loginFlower.png" alt=""></a>
-        <?php endif; ?>
-
     </nav>
+    <?php if ($isLoggedIn) : ?>
+        <div class="helloUserParent">
+            <p id="helloUser">Bonjour, <?= ($_SESSION['user']['email']) ?> </p>
+        </div>
+        <a href="/ctrl/login/logout.php" class="profile-icon"><img src="/asset/img/flowerYelow-removebg-preview.png" alt="Profile Icon"></a>
+    <?php else : ?>
+        <a href="/ctrl/login/login.php" class="profile-icon"><img src="/asset/img/loginFlower.png" alt="Login Icon"></a>
+    <?php endif; ?>
 </header>
 <script src="/asset/script/hiddenForm.js"></script>
-
