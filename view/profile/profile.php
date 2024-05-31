df115f278496cbfeaad1e4635cbae1879f6cd338
YAO <yamin13y@gmail.com>
2024-05-31 16:36:23 +0200

    [cleanup] tentative de nettoyer le code, et ajout de la page nos produits

--- view/profile/profile.php
+++ view/profile/profile.php
@@ -1,6 +1,6 @@
 <main>
     <section>
-        <h2>Avatar de <?= $_SESSION['user']['email'] ?></h2>
+        <h3 class="avatarTitle">Avatar de <?= $_SESSION['user']['email'] ?></h3>
         <p><img style="border-radius: 8%;" src="/upload/<?= $_SESSION['user']['avatar_filename'] ?>" /></p>
     </section>
 </main>
