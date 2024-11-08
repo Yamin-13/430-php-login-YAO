1b1c1c3005aa39dedce1baa876737a5c8d783312
yamin <yamin13y@gmail.com>
2024-11-08 19:42:36 +0100

    [fix] correction de texte

--- view/login/welcome.php
+++ view/login/welcome.php
@@ -1,5 +1,5 @@
 <main>
-    <h2 class="welcome">Welcome, <?= ($_SESSION['user']['email']) ?>!!!!</h2>
+    <h2 class="welcome">Bienvenue, <?= ($_SESSION['user']['email']) ?>!!!!</h2>
     <video autoplay muted loop playsinline id="bg-video">
         <source src="/asset/img/roseFlower.mp4" type="video/mp4">
     </video>
